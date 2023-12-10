eeb4f84a8d71d46e985c8f6cf72b6cdf0a8466c7
Leila <britezneira.leila@gmail.com>
2023-12-11 00:30:56 +0100

    Ultimas modificaciones

--- js/header.js
+++ js/header.js
@@ -3,6 +3,5 @@ document.getElementById("header").innerHTML= `
             <a class="text-white px-5" href="../index.html">Volver a Codo Burger</a>
             <a class="text-white px-5" href="personas.html">Tabla</a>
             <a class="text-white px-5" href="ingresar_persona.html">Registrar persona</a>
-            
 </nav>
 `
